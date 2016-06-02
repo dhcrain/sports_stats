61ac45b9d274e9baaaf8cce37121ab5716ef3f1c
dhcrain <dhcrain@gmail.com>
2016-06-02 17:12:02 -0400

    Making progress on selecting what you want to search for

--- search_bb-stats.py
+++ search_bb-stats.py
@@ -3,9 +3,10 @@ import psycopg2
 connection = psycopg2.connect("dbname=learning_sql user=dbperson")
 cursor = connection.cursor()
 
+
 def search_player_name():
-    search = input('Search: ')
-    cursor.execute("select * from san_jacinto_baseball WHERE full_name = %s;", (search,))
+    player_search = input('Search: ')
+    cursor.execute("select * from san_jacinto_baseball WHERE full_name = %s;", (player_search,))
     results = cursor.fetchall()
     for row in results:
         print(row[0], "#", row[2])
@@ -14,9 +15,43 @@ def search_player_name():
         print("HR:", row[4])
         print("RBI:", row[5])
         print("Runs:", row[6])
+    else:   # not quite right
+        print("1  ! No Match !")
+# search_player_name()
+
+
+def search_query():
+    print("Search by: 1. Full Name 2. Number 3. Position 4. Average etc....")
+    search_num = input('# to search by: ')
+    search_string = input("Search Criteria: ")
+    if search_num == "1":
+        search("full_name", search_string)
+    elif search_num == "2":
+        search("number", search_string)
+
+
+def search_display(row):
+    print(row[0], "#", row[2])
+    print("Pos.", row[1])
+    print("AVG:", row[3])
+    print("HR:", row[4])
+    print("RBI:", row[5])
+    print("Runs:", row[6])
+
+
+def search(stat, search_critera):
+    cursor.execute("select * from san_jacinto_baseball WHERE {} = %s;".format(stat), (search_critera,))
+    results = cursor.fetchall()
+    for row in results:
+        # return row
+        print(row[0], "#", row[2])
+        print("Pos.", row[1])
+        print("AVG:", row[3])
+        print("HR:", row[4])
+        print("RBI:", row[5])
+        print("Runs:", row[6])
     else:
-        print(" ! No Match !")
-search_player_name()
+        print("2 ! No Match !")
 
 
 def add_player():
@@ -39,9 +74,10 @@ connection.commit()
 # for row in results:
 #     print(row)
 
+search_query()
+
 cursor.close()
 connection.close()
-
 
 
 
