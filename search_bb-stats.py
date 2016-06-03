ed6bb6a2f11cec5a6367e57bc31a583d9ae5ac8a
dhcrain <dhcrain@gmail.com>
2016-06-03 12:00:11 -0400

    Can now edit a players stats

--- search_bb-stats.py
+++ search_bb-stats.py
@@ -1,70 +1,110 @@
+# ! ALL use of "{}".format() is hard coded and not available for user input !!!!!
+
 import psycopg2
 
 connection = psycopg2.connect("dbname=learning_sql user=dbperson")
 cursor = connection.cursor()
 
+print("\nWelcome to San Jacinto Baseball Stats")
 
-# Not Needed? redundat to search()?
-def search_player_name():
-    player_search = input('Search: ')
-    cursor.execute("select * from san_jacinto_baseball WHERE full_name = %s;", (player_search,))
-    results = cursor.fetchall()
-    for row in results:
-        print(row[0], "#", row[2])
-        print("Pos.", row[1])
-        print("AVG:", row[3])
-        print("HR:", row[4])
-        print("RBI:", row[5])
-        print("Runs:", row[6])
-    else:   # not quite right
-        print("1  ! No Match !")
+
+def welcome():
+    print("\nWhat would you like to do?")
+    choice = input("1. Search for a player\n2. Add a Player\n3. Update Player\n4. Show All\n5. Exit ")
+    options = {"1": search_query, "2": add_player, "3": update_player, "4": show_all, "5": exit}
+    if choice not in "1234":
+        print(" ! Not a valid choice ! \n")
+    else:
+        options[choice]()
+
+    welcome()
 
 
 def search_query():
-    print("Search by:\n1.Full Name 2.Number 3.Position 4.Average 5.HR 6.RBI 7.Runs")
-    search_num = input('# to search by: ')
+    print("\nSearch by:\n"
+          "1. Full Name\n"
+          "2. Number\n"
+          "3. Position\n"
+          "4. Average\n"
+          "5. HR\n"
+          "6. RBI\n"
+          "7. Runs")
+    search_num = input('What would you like to search by: ')
     search_string = input("Search Criteria: ")
-    if search_num == "1":
-        search("full_name", search_string)
-    elif search_num == "2":
-        search("number", search_string)
-    elif search_num == "3":
-        search("position", search_string)
-    elif search_num == "4":
-        search("avg", search_string)
-    elif search_num == "5":
-        search("hr", search_string)
-    elif search_num == "6":
-        search("rbi", search_string)
-    elif search_num == "7":
-        search("runs", search_string)
+    sq_options = {"1": "full_name",
+                  "2": "number",
+                  "3": "position",
+                  "4": "avg",
+                  "5": "hr",
+                  "6": "rbi",
+                  "7": "runs"}
+    if search_num not in "13":
+        print("Would you like you search to be:")
+        operator = input("1. Exact match\n"
+                         "2. Less Than\n"
+                         "3. Greater Than")
+        op_choice = {"1": "=", "2": "<", "3": ">"}
+        search(sq_options[search_num], op_choice[operator], search_string)
+    else:
+        search(sq_options[search_num], "=", search_string)
+    if input("Would you like to edit this player? Y/n").lower() != "n"
+        edit_palyer(search_string)
+    else:
+        welcome()
+    return search_string
 
-def search_display(row):
-    print(row[0], "#", row[2])
-    print("Pos.", row[1])
-    print("AVG:", row[3])
-    print("HR:", row[4])
-    print("RBI:", row[5])
-    print("Runs:", row[6])
 
-
-def search(stat, search_critera):
-    cursor.execute("select * from san_jacinto_baseball WHERE {} = %s;".format(stat), (search_critera,))
+def search(stat, operator, search_critera):
+    cursor.execute("select * from san_jacinto_baseball WHERE {} {} %s;".format(stat, operator), (search_critera,))
     results = cursor.fetchall()
     for row in results:
         search_display(row)
-        print(row[0], "#", row[2])
-        print("Pos.", row[1])
-        print("AVG:", row[3])
-        print("HR:", row[4])
-        print("RBI:", row[5])
-        print("Runs:", row[6])
-    else:   # funky
+    else:   # funky, always displays.....
         print("2 ! No Match !")
+    return search_critera
+
+
+def search_display(player):
+    print("-"*25)
+    print(player[0], "#", player[2])
+    print("Pos.", player[1])
+    print("AVG:", player[3])
+    print("HR:", player[4])
+    print("RBI:", player[5])
+    print("Runs:", player[6])
+
+
+def update_player():
+    return input("Which player to update? (Full Name): ")
+
+
+def edit_palyer(name):
+    search("full_name", "=", name)
+    update_num = input("\nWhat stat update? \n"
+                       "1. Full Name\n"
+                       "2. Number\n"
+                       "3. Position\n"
+                       "4. Average\n"
+                       "5. HR\n"
+                       "6. RBI\n"
+                       "7. Runs ")
+    update_options = {"1": "full_name",
+                      "2": "number",
+                      "3": "position",
+                      "4": "avg",
+                      "5": "hr",
+                      "6": "rbi",
+                      "7": "runs"}
+    new_value = input("What is the new value? ")
+    cursor.execute("UPDATE san_jacinto_baseball SET {} = %s WHERE full_name = %s;".format(update_options[update_num]), (new_value, name))
+    connection.commit()
+    print(" * Player Updated *")
+    search("full_name", "=", name)
+    welcome()
 
 
 def add_player():
-    print("Enter new player information")
+    print("\nEnter new player information")
     full_name = input("Full Name: ")
     position = input("Position (ex.INF): ").upper()
     number = int(input("Number: "))
@@ -78,20 +118,18 @@ def add_player():
     connection.commit()
     print(" * Player Successfully Added *")
 
+
+def show_all():
+    cursor.execute("select * from san_jacinto_baseball")
+    results = cursor.fetchall()
+    for row in results:
+        search_display(row)
+
 connection.commit()
 
-# cursor.execute("select * from san_jacinto_baseball")
-# results = cursor.fetchall()
-# for row in results:
-#     print(row)
 
-# search_query()
-add_player()
+welcome()
+# update_player()
 
 cursor.close()
 connection.close()
-
-
-
-
-
